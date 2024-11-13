7d843cf2f8d112bcc43e95347ee4b7bb94918021
mohamed <kittanyenterprise@gmail.com>
2024-11-13 19:06:34 +0200

    done

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, render_template, jsonify, request
 from app.model import chat_rooms
-#import datetime
-
 
 chat = Blueprint('chat', __name__)
+
 
 
 ############################### GET '/' ###############################
@@ -16,23 +15,18 @@ def index():
 ############################### POST /api/chat/<room> ###############################
 @chat.route('/api/chat/<room>', methods=['POST'])
 def post_message(room):
-    
     username = request.form.get('username')
     message = request.form.get('msg')
 
     if not username or not message:
         return jsonify({"error": "Both 'username' and 'message' fields are required"}), 400
     
-    #TODO: timestamp feature 
-    # timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # chat_line = f"[{timestamp}] {username}: {message}"
     chat_line = f"{username}: {message}"
     
     if room not in chat_rooms:
         chat_rooms[room] = [f"Room name: {room}"]
 
     chat_rooms[room].append(chat_line)
-    
     return jsonify({"status": "Message saved"}), 201
 
 
@@ -43,7 +37,6 @@ def get_chat(room):
         return jsonify({"error": "Room not found"}), 404
 
     chat_log = "\n".join(chat_rooms[room])
-
     return chat_log, 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
 
@@ -59,5 +52,3 @@ def index_room(room):
 
     # Pass room and chat_log to the template
     return render_template('index.html', room=room, chat_log=chat_log)
-
-
